54de09b9cc1dbc50d26f1e36549542e2e597bb58
Unknown <k.ballagher@gmail.com>
2018-10-13 14:17:19 +0100

    FaceRecognition, Clarifai integration, Functionality
    Setup Framework for FaceRecognition component.
    Imported Clarifai and setup face recognition api.
    Setup .bounding-box to generate bordered box around faces based on data from clarifai api.

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'f411376f68944771958b5b7f9862b6c4'
+ });
 
 const particlesOptions = {
   particles: {
@@ -22,6 +27,45 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),   
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    console.log('click');
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response))
+    .catch(err => console.log(err)));
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,8 +75,8 @@ class App extends Component {
         <Navigation /> 
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition />  */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
       </div>
     );
   }
