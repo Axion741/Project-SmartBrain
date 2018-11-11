66ced09d9982cefa4d15f19b2bb2f961a2b26e5c
Unknown <k.ballagher@gmail.com>
2018-11-11 09:57:11 +0000

    Fetch error

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -40,9 +40,6 @@ class App extends Component {
     }
   }
 
-  // componentDidMount() {
-  //   fetch('http:')
-  // }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
